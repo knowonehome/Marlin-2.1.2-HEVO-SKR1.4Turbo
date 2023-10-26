4a5dda290ae4c340a9992ba3bad340556a3a4609
knowonehome <knowonehome@gmail.com>
2023-10-26 16:44:30 -0500

    Update Configuration_adv.h
    Changed the final probe value to use the whole bed as well as the first one too.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2142,10 +2142,10 @@
  */
 #if EITHER(AUTO_BED_LEVELING_3POINT, AUTO_BED_LEVELING_UBL)
   #define PROBE_PT_1_X 20
-  #define PROBE_PT_1_Y 180
+  #define PROBE_PT_1_Y 300
   #define PROBE_PT_2_X 20
   #define PROBE_PT_2_Y 20
-  #define PROBE_PT_3_X 170
+  #define PROBE_PT_3_X 270
   #define PROBE_PT_3_Y 20
 #endif
 
